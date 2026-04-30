608110e05629318c65c35dc91349d5a5a753a9d0
JERICHO URBANO <jerichourbano.01.01.04@gmail.com>
2026-04-30 23:08:24 +0800

    updated for deploy

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jeric\Documents\Github\cimore_backend\venv
+set VIRTUAL_ENV=C:\Users\jeric\Documents\GitHub\CiMORe\CiMOReDeploy\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
